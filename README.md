e705a21bbfea7faf73de505c97e7379c71918954
Harish <Harishappana@gmail.com>
2025-08-04 21:58:24 +0530

    delete

--- README.md
+++
@@ -1,1 +0,0 @@
-# vmss-node
--- delete-all-rg.sh
+++
@@ -1,7 +0,0 @@
-
-# List all resource groups and delete them
-az group list --query "[].name" -o tsv | while read -r line
-do
-   echo "Deleting resource group $line"
-   az group delete --name "$line" --yes --no-wait
-done
